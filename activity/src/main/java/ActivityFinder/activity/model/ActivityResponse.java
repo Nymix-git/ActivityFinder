5d5f331f5df88959a6deb684aec9a9342d584e8d
Nymix-git <jakob@gridl.at>
2023-10-01 01:47:10 +0200

    Fixed the order of response attributes

--- activity/src/main/java/ActivityFinder/activity/model/ActivityResponse.java
+++ activity/src/main/java/ActivityFinder/activity/model/ActivityResponse.java
@@ -1,5 +1,12 @@
 package ActivityFinder.activity.model;
 
 // Record to write the data received from the bored API
-public record ActivityResponse(String activity, Double accessibility, String Type, Integer participants, Double price, String link, Long key) {
+public record ActivityResponse(String activity,
+                               String Type,
+                               Integer participants,
+                               Double price,
+                               String link,
+                               Long key,
+                               Double accessibility) {
+
 }
